5ee450742c78fb2f2d3fd5d62536ac134a538ff7
Rafael <faelleonan@gmail.com>
2020-05-10 10:49:15 -0300

    Evitando colisoes

--- foge_foge.rb
+++ foge_foge.rb
@@ -18,17 +18,18 @@ def encontra_jogador(mapa)
 end
 
 def calcula_nova_posicao(heroi, direcao)
+	nova_posicao = heroi.dup
 	case direcao
 			when "W"
-				heroi[0] -= 1
+				nova_posicao[0] -= 1
 			when "S"
-				heroi[0] += 1
+				nova_posicao[0] += 1
 			when "A"
-				heroi[1] -= 1
+				nova_posicao[1] -= 1
 			when "D"
-				heroi[1] += 1
+				nova_posicao[1] += 1
 		end
-		heroi
+		nova_posicao
 end
 
 def joga(nome)
@@ -37,8 +38,28 @@ def joga(nome)
 		desenha mapa
 		direcao = pede_movimento
 		heroi = encontra_jogador mapa
-		mapa[heroi[0]][heroi[1]] = " "
 		nova_posicao = calcula_nova_posicao heroi, direcao
+    
+    if nova_posicao[0] < 0
+    	next
+    end
+
+    if nova_posicao[1] < 0
+    	next
+    end
+
+    if nova_posicao[0] >= mapa.size
+    	next
+    end
+
+    if nova_posicao[1] >= mapa[0].size
+    	next
+    end
+
+		if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
+			next
+		end
+		mapa[heroi[0]][heroi[1]] = " "
 		mapa[nova_posicao[0]][nova_posicao[1]] = "H"
 	end
 end
