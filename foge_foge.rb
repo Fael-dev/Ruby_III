2f237a19da5ea1ee50bcb83fcef7c864e12da7a9
Rafael <faelleonan@gmail.com>
2020-05-10 10:13:58 -0300

    Movimentando personagem

--- foge_foge.rb
+++ foge_foge.rb
@@ -8,11 +8,10 @@ end
 
 def encontra_jogador(mapa)
 	heroi = "H"
-	for linha = 1..(mapa.size-1)
-		linha_atual = mapa[linha]
+	mapa.each_with_index do |linha_atual, linha|
 		coluna_heroi = linha_atual.index heroi 
 		if coluna_heroi
-			# ACHEI O JOGADOR
+			return [linha, coluna_heroi]
 		end
 	end
 	# NÃO ACHEI
@@ -20,10 +19,22 @@ end
 
 def joga(nome)
 	mapa = le_mapa 1
-
 	while true
 		desenha mapa
 		direcao = pede_movimento
+		heroi = encontra_jogador mapa
+		mapa[heroi[0]][heroi[1]] = " "
+		case direcao
+			when "W"
+				heroi[0] -= 1
+			when "S"
+				heroi[0] += 1
+			when "A"
+				heroi[1] -= 1
+			when "D"
+				heroi[1] += 1
+		end
+		mapa[heroi[0]][heroi[1]] = "H"
 	end
 end
 
