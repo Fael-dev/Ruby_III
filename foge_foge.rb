3b668e439be1f9983c16c0c43d71668673802220
Rafael <faelleonan@gmail.com>
2020-05-24 13:06:56 -0300

    Melhorando movimento dos fantasmas

--- foge_foge.rb
+++ foge_foge.rb
@@ -48,47 +48,56 @@ def posicao_valida?(mapa, posicao)
 	true
 end
 
-def move_fantasmas mapa
+def copia_mapa(mapa)
+	novo_mapa = mapa.join("\n").tr("F", " ").split("\n")
+end
+
+def move_fantasmas(mapa)
     fantasma = "F"
+    novo_mapa = copia_mapa mapa 
     mapa.each_with_index do |linha_atual, linha|
         linha_atual.chars.each_with_index do |caracter_atual, caracter|
             eh_fantasma = caracter_atual == fantasma
             if eh_fantasma
-                move_fantasma mapa,[linha,caracter]
+                move_fantasma mapa, novo_mapa, [linha,caracter]
             end
         end
     end
+    novo_mapa
 end
 
-def move_fantasma (mapa, posicao)
-    posicoes = posicoes_validas_a_partir_de mapa, posicao
-    return if posicoes.empty?
+def move_fantasma (mapa, novo_mapa, posicao)
+		
+    posicoes = posicoes_validas_a_partir_de mapa, novo_mapa, posicao
+    if posicoes.empty?
+    	return 
+    end
     nova_posicao = posicoes[0]
 
     mapa[posicao[0]][posicao[1]] = " "
-    mapa[nova_posicao[0]][nova_posicao[1]] = "F"
+    novo_mapa[nova_posicao[0]][nova_posicao[1]] = "F"
 end
 
-def posicoes_validas_a_partir_de(mapa, posicao)
+def posicoes_validas_a_partir_de(mapa, novo_mapa, posicao)
     posicoes = []
 
     baixo = [posicao[0] + 1, posicao[1]]
-    if posicao_valida? mapa, baixo
+    if posicao_valida?(mapa, baixo) && posicao_valida?(novo_mapa, baixo)
         posicoes << baixo
     end
 
     direita = [posicao[0], posicao[1] + 1]
-    if posicao_valida? mapa,direita
+    if posicao_valida?(mapa,direita) && posicao_valida?(novo_mapa, direita)
         posicoes << direita
     end
 
     cima = [posicao[0] - 1, posicao[1]]
-    if posicao_valida? mapa,cima
+    if posicao_valida?(mapa,cima) && posicao_valida?(novo_mapa, cima)
         posicoes << cima
     end
 
     esquerda = [posicao[0], posicao[1] - 1]
-    if posicao_valida? mapa,esquerda
+    if posicao_valida?(mapa,esquerda) && posicao_valida?(novo_mapa,esquerda)
         posicoes << esquerda
     end
 
@@ -109,7 +118,7 @@ def joga(nome)
     
 		mapa[heroi[0]][heroi[1]] = " "
 		mapa[nova_posicao[0]][nova_posicao[1]] = "H"
-		move_fantasmas mapa
+		mapa = move_fantasmas mapa
 	end
 end
 
